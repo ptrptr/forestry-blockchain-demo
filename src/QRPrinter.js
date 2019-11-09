ce14c53972cd421efab30c4284435494a19b0945
Petri Sarvamaa <ptrptrdev@gmail.com>
2019-11-09 18:42:34 +0200

    Printer formatting improvements

--- src/QRPrinter.js
+++ src/QRPrinter.js
@@ -7,18 +7,18 @@ export default function QRPrinter() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-        <QRCode value="Hello world!" level="M"></QRCode>
+        <QRCode value="Content" level="M"></QRCode>
         </a>
+        <br/>
+        <br/>
+        <br/>
+        <p>Content</p>
       </header>
     </div>
   );
