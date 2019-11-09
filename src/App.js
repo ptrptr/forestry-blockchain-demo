e8e78f09440fe11ec316317e70c59fdb017d9538
Petri Sarvamaa <ptrptrdev@gmail.com>
2019-11-09 16:53:58 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import QRPrinter from './QRPrinter';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import QRCode from 'qrcode.react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <QRCode value="Hello world!" level="M"></QRCode>
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/printer">Printer</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/printer">
+            <QRPrinter />
+          </Route>
+          <Router path="/">
+            <QRPrinter />
+          </Router>
+        </Switch>
+      </div>
+    </Router>        
+    );
 }
-
-export default App;
---
+++ src/QRPrinter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import QRCode from 'qrcode.react';
+import logo from './logo.svg';
+import './App.css';
+
+export default function QRPrinter() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <QRCode value="Hello world!" level="M"></QRCode>
+        </a>
+      </header>
+    </div>
+  );
+}
