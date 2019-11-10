69b9ec71be6af3b04cba5b1610446558203cb891
Petri Sarvamaa <ptrptrdev@gmail.com>
2019-11-10 11:32:17 +0200

    Add intro page text

--- src/Intro.js
+++ src/Intro.js
@@ -7,6 +7,9 @@ export default function Intro(props) {
     <div>
       <h2>Forestry blockchain demo</h2>
       <a href="/issue">Issue new raw material codes</a>
+      <p>Start by clicking the link above to issue 10 new initial codes. View object data by clicking on the QR codes.
+        Object data view also allows creating new subproducts or viewing history of products</p>
+        <p>Reset button resets the state of the application.</p>
       <div>
       <button onClick={reset}>Reset</button>
       </div>
