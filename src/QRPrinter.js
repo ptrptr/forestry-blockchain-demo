4d3883739e9aff213dc8e82d85edcbe2f3888bc0
Petri Sarvamaa <ptrptrdev@gmail.com>
2019-11-10 09:43:14 +0200

    Print object data fields in printer

--- src/QRPrinter.js
+++ src/QRPrinter.js
@@ -2,18 +2,28 @@ import React from 'react';
 import QRCode from 'qrcode.react';
 import logo from './logo.svg';
 import './App.css';
+import {find} from './HashStore';
 
 export default function QRPrinter(props) {
     const hash = props.match.params.hash;
+    const o = find(hash);
   return (
-    <div className="App">
-      <header className="App-header">
-        <QRCode value={hash} level="M"></QRCode>
-        <br/>
-        <br/>
-        <br/>
-        <p>{hash}</p>
-      </header>
+    <div>
+      <table>
+        {Object.entries(o).map((entry,index)=> {
+          if(entry[0][0] != '_') {
+            return <tr><td>{entry[0]}:</td><td>{JSON.stringify(entry[1])}</td></tr>
+          }
+        })}
+
+      </table>
+      <div>
+          <QRCode value={hash} level="M"></QRCode>
+          <br/>
+          <br/>
+          <br/>
+          <p>{hash}</p>
+      </div>
     </div>
   );
 }
